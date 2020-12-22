dd1e1aade332c168a42cdb6a1e64098769762d04
Jion-Andal <andaljion@gmail.com>
2020-12-22 15:07:49 +0800

    Changed file extensions

--- areas/detail.php
+++ areas/detail.php
@@ -17,11 +17,11 @@
 
 <body>
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="areas/index.html">Contact Tracing Platform</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="areas/index.php">Contact Tracing Platform</a>
     <span class="navbar-organizer w-100">{insert user name}</span>
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-            <a class="nav-link" id="logout" href="index.html">Sign out</a>
+            <a class="nav-link" id="logout" href="index.php">Sign out</a>
         </li>
     </ul>
 </nav>
@@ -31,21 +31,21 @@
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
             <div class="sidebar-sticky">
                 <ul class="nav flex-column">
-                    <li class="nav-item"><a class="nav-link" href="areas/index.html">Manage Contact Tracings</a></li>
+                    <li class="nav-item"><a class="nav-link" href="areas/index.php">Manage Contact Tracings</a></li>
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                     <span>{insert area name}</span>
                 </h6>
                 <ul class="nav flex-column">
-                    <li class="nav-item"><a class="nav-link active" href="areas/detail.html">Overview</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="areas/detail.php">Overview</a></li>
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                     <span>Reports</span>
                 </h6>
                 <ul class="nav flex-column mb-2">
-                    <li class="nav-item"><a class="nav-link" href="reports/index.html">Room capacity</a></li>
+                    <li class="nav-item"><a class="nav-link" href="reports/index.php">Room capacity</a></li>
                 </ul>
             </div>
         </nav>
@@ -56,7 +56,7 @@
                     <h1 class="h2">{insert area name}</h1>
                     <div class="btn-toolbar mb-2 mb-md-0">
                         <div class="btn-group mr-2">
-                            <a href="areas/edit.html" class="btn btn-sm btn-outline-secondary">Edit area</a>
+                            <a href="areas/edit.php" class="btn btn-sm btn-outline-secondary">Edit area</a>
                         </div>
                     </div>
                 </div>
@@ -77,7 +77,7 @@
                         <th>Date</th>
                         <th>Time</th>
                         <th>Person</th>
-                        
+
                     </tr>
                     </thead>
                     <tbody>
@@ -87,17 +87,17 @@
                     <tr>
                         <td class="text-nowrap"></td>
                         <td class="text-nowrap">08:30</td>
-                        <td>Juan Dela Cruz</td> 
+                        <td>Juan Dela Cruz</td>
                     </tr>
                     <tr class="positive">
                         <td class="text-nowrap"></td>
                         <td class="text-nowrap">10:32</td>
-                        <td><a href="people/details.html">Pepito Manaloto</a></td> 
+                        <td><a href="people/details.php">Pepito Manaloto</a></td>
                     </tr>
                     <tr>
                         <td class="text-nowrap"></td>
                         <td class="text-nowrap">11:43</td>
-                        <td>Captain Barbel</td> 
+                        <td>Captain Barbel</td>
                     </tr>
                     <tr>
                         <td class="text-nowrap" colspan="3">November 5, 2020</td>
@@ -105,19 +105,19 @@
                     <tr class="positive">
                         <td class="text-nowrap"></td>
                         <td class="text-nowrap">09:30</td>
-                        <td><a href="people/details.html">Pepito Manaloto</a></td> 
+                        <td><a href="people/details.php">Pepito Manaloto</a></td> 
                     </tr>
                     <tr>
                         <td class="text-nowrap"></td>
                         <td class="text-nowrap">16:21</td>
-                        <td>Captain Barbel</td> 
+                        <td>Captain Barbel</td>
                     </tr>
                     <tr>
                         <td class="text-nowrap"></td>
                         <td class="text-nowrap">17:14</td>
-                        <td>Juan Dela Cruz</td> 
+                        <td>Juan Dela Cruz</td>
                     </tr>
-                    
+
                     </tbody>
                 </table>
             </div>
